f3b0ecfec99673d2b776b1bed0142e97bb4efacf
mynkprtp <itsmayankhere24@gmail.com>
2024-05-28 21:07:43 +0530

    base url added in env variables

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import queryString from 'query-string'
 
-const baseUrl = 'http://127.0.0.1:5000/api/v1/'
+const baseUrl = process.env.REACT_APP_BASE_URL
+console.log(baseUrl)
 const getToken = () => localStorage.getItem('token')
 
 const axiosClient = axios.create({
