d7c47119afabe978d5569b63c351b547cfdfcddc
mynkprtp <itsmayankhere24@gmail.com>
2024-05-31 14:15:29 +0530

    Cors changes reverted

--- server/app.js
+++ server/app.js
@@ -6,24 +6,13 @@ const cors = require('cors')
 
 const app = express();
 
-const corsOptions = {
-    origin: process.env.FRONTEND_URL, // Replace with your React app's origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-};
-
-// Use the cors middleware
-app.use(cors(corsOptions));
-
-// Handle preflight requests for all routes
-app.options('*', cors(corsOptions));
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/v1', require('./src/v1/routes'));
+app.use('/api/v1/', require('./src/v1/routes'));
 
 module.exports = app;
